de540f80aa38ca56af1941856c850119182f7501
Sudheer <sudheer@vsccapital.in>
2026-08-25 19:16:25 +0530

    feat: add Microsoft Clarity analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -119,6 +119,16 @@ export default function RootLayout({
             gtag('config', 'G-F2SZMY24JR');
           `}
         </Script>
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=gtm";
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "y7xa29y4go");
+          `}
+        </Script>
         <MotionProvider>
           <Navbar />
           {children}
